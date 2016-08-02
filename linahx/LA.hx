b40f860aee6df22d2eabb8f27dbfc0ab29326056
Urs Stutz <gitter@ls3d.de>
2016-08-02 16:53:31 +0200

    transpose, dot

--- linahx/LA.hx
+++ linahx/LA.hx
@@ -6,13 +6,36 @@ package linahx;
  */
 class LA {
 
-	public static function eye( n:Int ):Matrix<Int> {
+	public static function zeros( rows:Int, columns:Int ):Matrix {
+		return new Matrix( rows, columns, 0 );
+	}
+	
+	public static function ones( rows:Int, columns:Int ):Matrix {
+		return new Matrix( rows, columns, 1 );
+	}
+	
+	public static function eye( n:Int ):Matrix {
 		
-		var e = new Matrix<Int>( n, n, 0 );
+		var e = new Matrix( n, n, 0 );
 		for ( i in 0...n ) {
 			e.data[i][i] = 1;
 		}
 		return e;
 	}
 	
+	public static function rand( rows:Int, columns:Int ):Matrix {
+		
+		var r = new Matrix( rows, columns, 0.0 );
+		for ( row in 0...r.rows ) {
+			for ( column in 0...r.columns ) {
+				r.data[row][column] = Math.random();
+			}
+		}
+		return r;
+	}
+	
+	public static function dot( matrix1:Matrix, matrix2:Matrix ):Matrix {
+		
+		return matrix1.dot( matrix2 );
+	}
 }
--- linahx/Matrix.hx
+++ linahx/Matrix.hx
@@ -5,40 +5,76 @@ import haxe.ds.Vector;
  * ...
  * @author Urs Stutz
  */
-class Matrix<T> {
+class Matrix {
 	
-	public var columns:Int;
 	public var rows:Int;
+	public var columns:Int;
 	
-	public var data:Vector<Vector<T>>;
+	public var data:Vector<Vector<Float>>;
 
-	public function new( columns:Int, rows:Int, defaultvalue:T ) {
+	public function new( rows:Int, columns:Int, ?defaultvalue:Float ) {
+		
 		this.rows = rows;
 		this.columns = columns;
 		
-		data = new Vector<Vector<T>>(columns);
-		for ( column in 0...columns ) {
-			data[column] = new Vector<T>(rows);
-			for ( row in 0...rows ) {
-				data[column][row] = defaultvalue;
+		data = new Vector<Vector<Float>>( rows );
+		for ( row in 0...rows ) {
+			data[row] = new Vector<Float>(columns);
+			
+			if( defaultvalue != null ) {
+				for ( column in 0...columns ) {
+					data[row][column] = defaultvalue; //trace( "row " + row + "  column " + column + "  set to " + defaultvalue );
+				}
 			}
 		}
 	}
 	
+	public function transpose():Matrix {
+		
+		var transposedColumns = rows;
+		var transposedRows = columns;
+		
+		var transposedMatrix = new Matrix( transposedColumns, transposedRows );
+		for ( column in 0...transposedColumns ) {
+			for ( row in 0...transposedRows ) {
+				transposedMatrix.data[column][row] = data[row][column];
+			}
+		}
+		return transposedMatrix;
+	}
 	
+	public function dot( otherMatrix:Matrix ):Matrix {
+		
+		if ( columns != otherMatrix.rows ) {
+			throw "Error: matrix must have " + columns + " rows.";
+		}
+		
+		var resultMatrix = new Matrix(columns, otherMatrix.rows );
+		
+		var sum = 0.0;
+		for ( column in 0...columns ) {
+			for ( oRow in 0...otherMatrix.rows ) {
+				sum += data[0][column] * otherMatrix.data[oRow][0];
+			}
+		}
+		resultMatrix.data[0][0] = sum;
+		
+		return resultMatrix;
+	}
 	
 	public function toString():String {
 		
 		var s = "[";
-		for ( column in 0...columns ) {
-			s += column == 0 ? "[" : " [";
-			for( row in 0...rows ) {
-				s += Std.string( data[column][row] ) + "   "; 
+		for ( row in 0...rows ) {
+			s += row == 0 ? "[" : " [";
+			for( column in 0...columns ) {
+				s += Std.string( data[row][column] ) + ( column<columns-1 ? "   " : "" ); 
 			}
-			s += column == columns-1 ? "]" : "]\n";
+			s += row == rows-1 ? "]" : "]\n";
 		}
 		s += "]\n";
 		
 		return s;
 	}
+	
 }
